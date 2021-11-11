e66f34cf13dadb11286d2ca04f1c2c1cc03534e2
Stephen Montague <stephen.d.montague@gmail.com>
2021-11-11 07:08:11 -0500

    This function logs xxx b/c the variables aren't named well.

--- main.go
+++ main.go
@@ -5,9 +5,14 @@ import "log"
 var s = "seven"
 
 func main() {
-	log.Println(s)
+	var s2 = "six"
+	log.Println("s is", s)
+	log.Println("s2 is", s2)
+
+	saySomething("xxx") // Which s is this going to refer to?
 }
 
 func saySomething(s string) (string, string) {
+	log.Println("s from the saySomething func is", s)
 	return s, "World"
 }
