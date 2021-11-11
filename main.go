92d5278ce41e4ce908be9d4f3d552b4ea48cc60e
Stephen Montague <me@montaguestephen.com>
2021-11-11 08:47:51 -0500

    Learning basics of map with a string map

--- main.go
+++ main.go
@@ -2,23 +2,10 @@ package main
 
 import "log"
 
-type myStruct struct {
-	FirstName string
-}
-
-// (m *myStruct) is a receiver that associates the function below to myStruct
-func (m *myStruct) printFirstName() string {
-	return m.FirstName
-}
-
 func main() {
-	var myVar myStruct
-	myVar.FirstName = "John"
+	myMap := make(map[string]string)
 
-	myVar2 := myStruct{
-		FirstName: "Mary",
-	}
+	myMap["dog"] = "Pocus"
 
-	log.Println("myVar is set to", myVar.printFirstName())
-	log.Println("myVar2 is set to", myVar2.printFirstName())
+	log.Println(myMap["dog"])
 }
