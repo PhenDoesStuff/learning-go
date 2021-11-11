302dee2ae4b2e651ee5671d24bb722a9d88d86a6
Stephen Montague <stephen.d.montague@gmail.com>
2021-11-11 07:46:16 -0500

    Using receivers to associate functions with structures. Now any structure of myStruct can use the printFirstName function

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import "log"
 
+type myStruct struct {
+	FirstName string
+}
+
+// (m *myStruct) is a receiver that associates the function below to myStruct
+func (m *myStruct) printFirstName() string {
+	return m.FirstName
+}
+
+func main() {
+	var myVar myStruct
+	myVar.FirstName = "John"
+
+	myVar2 := myStruct{
+		FirstName: "Mary",
+	}
+
+	log.Println("myVar is set to", myVar.printFirstName())
+	log.Println("myVar2 is set to", myVar2.printFirstName())
 }
