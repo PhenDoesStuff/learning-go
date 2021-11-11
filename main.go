a10164e6c0e7b6a8b1aa6d8d3d7406d0711d2a37
Stephen Montague <stephen.d.montague@gmail.com>
2021-11-11 06:39:00 -0500

    Learned the basics of variables and functions

--- main.go
+++ main.go
@@ -4,4 +4,22 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, world.")
+
+	var whatToSay string
+	var i int
+
+	whatToSay = "Goodbye, cruel world"
+	fmt.Println(whatToSay)
+
+	i = 7
+
+	fmt.Println("i is set to:", i)
+
+	whatWasSaid, otherThing := saySomething()
+
+	fmt.Println("The function returned:", whatWasSaid, otherThing)
+}
+
+func saySomething() (string, string) {
+	return "something", "something else"
 }
