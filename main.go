c6a4ef1e3f7cb73330faa93081203c5fc120c2f4
Stephen Montague <me@montaguestephen.com>
2021-11-11 09:22:52 -0500

    Learning switch statements

--- main.go
+++ main.go
@@ -3,10 +3,19 @@ package main
 import "log"
 
 func main() {
-	myNum := 100
-	isTrue := false
+	myVar := "cat"
 
-	if myNum > 99 && isTrue {
-		log.Println("myNum is greater than 99 and isTrue is set to true")
+	switch myVar {
+	case "cat":
+		log.Println("cat is set to cat")
+
+	case "dog":
+		log.Println("cat is set to dog")
+		
+	case "fish":
+		log.Println("cat is set to fish")
+
+	default:
+		log.Println("cat is somethinge else")
 	}
 }
