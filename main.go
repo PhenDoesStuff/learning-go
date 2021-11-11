b3e8e771b243842a4b7627aad9ca93d78f42a6de
Stephen Montague <stephen.d.montague@gmail.com>
2021-11-11 07:39:42 -0500

    Starting from scratch to see how functions are related to a structure

--- main.go
+++ main.go
@@ -1,25 +1,5 @@
 package main
 
-import (
-	"log"
-	"time"
-)
-
-type User struct {
-	FirstName string
-	LastName string
-	PhoneNumber string
-	Age int
-	BirthDate time.Time
-}
-
 func main() {
-	user := User {
-		FirstName: "Stephen",
-		LastName: "Montague",
-		PhoneNumber: "123-456-7890",
-		Age: 26,
-	}
 
-	log.Println(user.FirstName, user.LastName, "Birthdate:", user.BirthDate)
 }
