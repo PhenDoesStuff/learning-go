0a4ef766775ce3f66bd264ec417334afe65abe87
Stephen Montague <me@montaguestephen.com>
2021-11-11 19:59:27 -0500

    Here we can easily loop over the User struct that we created.

--- main.go
+++ main.go
@@ -1,16 +1,22 @@
 package main
 
-import (
-	"log"
-)
+import "log"
 
 func main() {
-	animals := make(map[string]string)
+	type User struct {
+		FirstName string
+		LastName  string
+		Email     string
+		Age       int
+	}
 
-	animals["dog"] = "fido"
-	animals["cat"] = "fluffy"
+	var users []User
+	users = append(users, User{"John", "Smith", "John@Smith.com", 30})
+	users = append(users, User{"Mary", "Jones", "Mary@Jones.com", 25})
+	users = append(users, User{"Sally", "Brown", "Sally@Brown.com", 12})
+	users = append(users, User{"Alex", "Anderson", "Alex@Anderson.com", 17})
 
-	for animalType, animal := range animals {
-		log.Println(animalType, animal)
+	for _, l := range users {
+		log.Println(l.FirstName, l.LastName, l.Email, l.Age)
 	}
 }
